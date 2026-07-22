568223571cec58337dc8a9caf0f399fb2fd692f1
CRYSTAL-LEVI UZOUKWU <crysnovax@gmail.com>
2026-07-22 14:19:14 +0100

    [new]

--- settings/config.js
+++ settings/config.js
@@ -102,7 +102,8 @@ const config = {
         getVar('THUMB_URL')          ||
         userConfig?.thumbUrl         ||
     //    'https://cdn.crysnovax.link/files/1783469167623-6d58c43c-68b4-41ce-87ab-c0da1f615b28.mp4',
-        'https://cdn.crysnovax.link/files/1778529162616-eca99707-7b11-453a-802a-e85a9d1c2395.jpeg',
+       // 'https://cdn.crysnovax.link/files/1778529162616-eca99707-7b11-453a-802a-e85a9d1c2395.jpeg',
+        'https://cdn.crysnovax.link/files/1784725819117-db2035e8-f797-4f47-8d41-e1dd6f546553.mp4',
 
     // ════════════════════════════════════════════
     // PANEL CONNECTOR API (CODY)
